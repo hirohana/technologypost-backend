864dd0a81defeaca885c92839385f8ebfda7f551
hiro <hirohanahaba@gmail.com>
2022-06-05 18:42:59 +0900

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: "https://hirohana-frontend.herokuapp.com/", //アクセス許可するオリジン
+    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+    optionsSuccessStatus: 200, //レスポンスstatusを200に設定
+  })
+);
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
