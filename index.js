3deb58f483a0db1e3a13e46bbc754314e45e5135
hiro <hirohanahaba@gmail.com>
2022-06-05 19:08:11 +0900

    secound commit

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
 
-app.use(
-  cors({
-    origin: "https://hirohana-frontend.herokuapp.com/", //アクセス許可するオリジン
-    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
-    optionsSuccessStatus: 200, //レスポンスstatusを200に設定
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://hirohana-frontend.herokuapp.com/", //アクセス許可するオリジン
+//     credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+//     optionsSuccessStatus: 200, //レスポンスstatusを200に設定
+//   })
+// );
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
